c977c398ff6ed5b182d6fff58e873381aa4b98d1
vinayankk <42730301+vinayankk@users.noreply.github.com>
2018-08-31 15:08:56 +0530

    reading analog valus from ADC1

--- ADC/ADC.c
+++ ADC/ADC.c
@@ -12,13 +12,8 @@
 GPIO_InitTypeDef GPIO_InitStructure;
 ADC_InitTypeDef ADC_InitStructure;
 
-void adc_init(void)
+void adc1_init(void)
 {
-	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AIN; //channel 1 ADC 1
-	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1 ;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-
 	RCC_ADCCLKConfig (RCC_PCLK2_Div6); //providing clock =72/6
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 
@@ -27,8 +22,8 @@ void adc_init(void)
 	ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
 	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-	ADC_InitStructure.ADC_NbrOfChannel = 1;
-	ADC_RegularChannelConfig(ADC1,ADC_Channel_1, 1,ADC_SampleTime_28Cycles5);
+	ADC_InitStructure.ADC_NbrOfChannel = 2;
+	ADC_RegularChannelConfig(ADC1,ADC_Channel_0, 1,ADC_SampleTime_28Cycles5); //pa0=0 pa7=7
 	ADC_Init ( ADC1, &ADC_InitStructure);
 
 	ADC_Cmd (ADC1,ENABLE); //enabling ADC
@@ -36,7 +31,7 @@ void adc_init(void)
 }
 
 
-int adc_calib()
+int adc1_calib()
 {
 	ADC_ResetCalibration(ADC1); //calibrating ADC
 	while(ADC_GetResetCalibrationStatus(ADC1));
@@ -45,10 +40,11 @@ int adc_calib()
 }
 
 
-int adc_readval(void)
+int adc1_readval(int channel) //upto 9 channels
 {
+		ADC_RegularChannelConfig(ADC1,channel,1,ADC_SampleTime_28Cycles5);
 	    ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 	    while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
-		int j = ADC_GetConversionValue(ADC1);
-		return(j);
+		int val = ADC_GetConversionValue(ADC1);
+		return(val);
 }
